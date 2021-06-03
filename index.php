1c61c999d230d2db93dd1348ba21762643ace577
Yosan Kiflemariam <yosankiflemariam@gmail.com>
2021-06-03 22:01:23 +0000

    Adds php file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+if(isset($_GET['FirstName'])){//show data
+  echo $_GET['FirstName'];
+}else{//show form
+  echo '
+  <form action="">
+First Name: <input type="text" name="FirstName" /><br />
+<input type="submit" />
+ </form>
+
+  ';
+}
